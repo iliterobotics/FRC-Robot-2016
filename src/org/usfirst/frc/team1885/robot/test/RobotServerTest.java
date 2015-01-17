3f76973689be68c9f061fa54ce84ffc2fef289ca
kianmehrabani <kianmehrabani@gmail.com>
2015-01-17 12:03:48 -0500

    added RobotServerTest

---
+++ src/org/usfirst/frc/team1885/robot/test/RobotServerTest.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team1885.robot.test;
+
+import org.usfirst.frc.team1885.robot.comms.RobotServer;
+import org.usfirst.frc.team1885.robot.comms.RobotServerEvent;
+import org.usfirst.frc.team1885.robot.comms.RobotServerListener;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class RobotServerTest extends SampleRobot implements RobotServerListener {
+    private RobotServer robotServer;
+    public RobotServerTest() {	
+    	this.robotServer = robotServer.getInstance();
+    	this.robotServer.setup(4444);
+    	this.robotServer.addListener( this );
+    	if ( this.robotServer.startServer() ) {
+    		System.out.println("Robot::Constructor - Successfully started data server!");
+    	}
+    }    
+   
+    public void receivedServerEvent( RobotServerEvent event ) {
+    	System.out.println( event.getMessage() );
+    }
+
+}
