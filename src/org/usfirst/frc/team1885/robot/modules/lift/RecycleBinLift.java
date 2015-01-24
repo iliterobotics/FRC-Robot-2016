5aec9d01a00516aec7d6c014136ac271b2559af0
Noah Reilly <noahreilly08@gmail.com>
2015-01-24 09:45:34 -0500

    refresh

--- src/org/usfirst/frc/team1885/robot/modules/lift/RecycleBinLift.java
+++ src/org/usfirst/frc/team1885/robot/modules/lift/RecycleBinLift.java
@@ -5,35 +5,37 @@ import org.usfirst.frc.team1885.robot.common.type.SensorType;
 import org.usfirst.frc.team1885.robot.input.SensorInputControl;
 
 public class RecycleBinLift {
-	
+
 	private MotorState state;
 	private boolean hasBin;
 
-	public RecycleBinLift( MotorState state ) {
+	public RecycleBinLift(MotorState state) {
 		this.state = state;
 		hasBin = false;
 	}
-	public void setState( MotorState state ){
+	public void setState(MotorState state) {
 		this.state = state;
 	}
 	public MotorState getMotorState() {
 		return state;
 	}
-	public boolean hasBin(){
+	public boolean hasBin() {
 		return hasBin;
 	}
-	public void updateLift(){
-		if( state == MotorState.UP ){
-			if( SensorInputControl.getInstance().getLimitSwitch( SensorType.RECYCLE_BIN_UPPER_LIMIT ).get() ){
+	public void updateLift() {
+		if (state == MotorState.UP) {
+			if (SensorInputControl.getInstance()
+					.getLimitSwitch(SensorType.RECYCLE_BIN_UPPER_LIMIT).get()) {
 				hasBin = true;
 				state = MotorState.STOP;
 			}
 		}
-		if( state == MotorState.DOWN ){
-			if( SensorInputControl.getInstance().getLimitSwitch( SensorType.RECYCLE_BIN_LOWER_LIMIT ).get() ){
+		if (state == MotorState.DOWN) {
+			if (SensorInputControl.getInstance()
+					.getLimitSwitch(SensorType.RECYCLE_BIN_LOWER_LIMIT).get()) {
 				hasBin = false;
 				state = MotorState.STOP;
 			}
-		}	
+		}
 	}
 }
