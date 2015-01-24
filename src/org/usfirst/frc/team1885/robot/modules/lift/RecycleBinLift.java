326a8a474db8531933a6a8ea4ddf40a69f0b30fe
Noah Reilly <noahreilly08@gmail.com>
2015-01-24 09:51:19 -0500

    refresh

--- src/org/usfirst/frc/team1885/robot/modules/lift/RecycleBinLift.java
+++ src/org/usfirst/frc/team1885/robot/modules/lift/RecycleBinLift.java
@@ -6,36 +6,36 @@ import org.usfirst.frc.team1885.robot.input.SensorInputControl;
 
 public class RecycleBinLift {
 
-	private MotorState state;
-	private boolean hasBin;
+    private MotorState state;
+    private boolean hasBin;
 
-	public RecycleBinLift(MotorState state) {
-		this.state = state;
-		hasBin = false;
-	}
-	public void setState(MotorState state) {
-		this.state = state;
-	}
-	public MotorState getMotorState() {
-		return state;
-	}
-	public boolean hasBin() {
-		return hasBin;
-	}
-	public void updateLift() {
-		if (state == MotorState.UP) {
-			if (SensorInputControl.getInstance()
-					.getLimitSwitch(SensorType.RECYCLE_BIN_UPPER_LIMIT).get()) {
-				hasBin = true;
-				state = MotorState.STOP;
-			}
-		}
-		if (state == MotorState.DOWN) {
-			if (SensorInputControl.getInstance()
-					.getLimitSwitch(SensorType.RECYCLE_BIN_LOWER_LIMIT).get()) {
-				hasBin = false;
-				state = MotorState.STOP;
-			}
-		}
-	}
+    public RecycleBinLift(MotorState state) {
+        this.state = state;
+        hasBin = false;
+    }
+    public void setState(MotorState state) {
+        this.state = state;
+    }
+    public MotorState getMotorState() {
+        return state;
+    }
+    public boolean hasBin() {
+        return hasBin;
+    }
+    public void updateLift() {
+        if (state == MotorState.UP) {
+            if (SensorInputControl.getInstance()
+                    .getLimitSwitch(SensorType.RECYCLE_BIN_UPPER_LIMIT).get()) {
+                hasBin = true;
+                state = MotorState.STOP;
+            }
+        }
+        if (state == MotorState.DOWN) {
+            if (SensorInputControl.getInstance()
+                    .getLimitSwitch(SensorType.RECYCLE_BIN_LOWER_LIMIT).get()) {
+                hasBin = false;
+                state = MotorState.STOP;
+            }
+        }
+    }
 }
--- src/org/usfirst/frc/team1885/robot/modules/lift/ToteLift.java
+++ src/org/usfirst/frc/team1885/robot/modules/lift/ToteLift.java
@@ -6,41 +6,41 @@ import org.usfirst.frc.team1885.robot.input.SensorInputControl;
 
 public class ToteLift {
 
-	private MotorState state;
-	//temp val for the maximum number of totes we can carry
-	private final int MAX_TOTE_COUNT = 6;
-	private int toteCount;
+    private MotorState state;
+    // temp val for the maximum number of totes we can carry
+    private final int MAX_TOTE_COUNT = 6;
+    private int toteCount;
 
-	public ToteLift(MotorState state) {
-		this.state = state;
-		toteCount = 0;
-	}
-	public void setMotorState(MotorState state) {
-		this.state = state;
-	}
-	public MotorState getMotorState() {
-		return state;
-	}
-	public int getToteCount() {
-		return toteCount;
-	}
-	public void updateManualLift() {
-		if (state == MotorState.UP) {
-			if (toteCount == MAX_TOTE_COUNT) {
-				state = MotorState.STOP;
-			}
-			if (SensorInputControl.getInstance()
-					.getLimitSwitch(SensorType.TOTE_LEFT_LIMIT_SWITCH).get()) {
-				toteCount++;
-			}
-		}
-		if (state == MotorState.DOWN) {
-			if (SensorInputControl.getInstance()
-					.getLimitSwitch(SensorType.TOTE_RIGHT_LIMIT_SWITCH).get()) {
-				if (toteCount > 0) {
-					toteCount--;
-				}
-			}
-		}
-	}
+    public ToteLift(MotorState state) {
+        this.state = state;
+        toteCount = 0;
+    }
+    public void setMotorState(MotorState state) {
+        this.state = state;
+    }
+    public MotorState getMotorState() {
+        return state;
+    }
+    public int getToteCount() {
+        return toteCount;
+    }
+    public void updateManualLift() {
+        if (state == MotorState.UP) {
+            if (toteCount == MAX_TOTE_COUNT) {
+                state = MotorState.STOP;
+            }
+            if (SensorInputControl.getInstance()
+                    .getLimitSwitch(SensorType.TOTE_LEFT_LIMIT_SWITCH).get()) {
+                toteCount++;
+            }
+        }
+        if (state == MotorState.DOWN) {
+            if (SensorInputControl.getInstance()
+                    .getLimitSwitch(SensorType.TOTE_RIGHT_LIMIT_SWITCH).get()) {
+                if (toteCount > 0) {
+                    toteCount--;
+                }
+            }
+        }
+    }
 }
