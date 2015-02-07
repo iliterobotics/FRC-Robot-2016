de14a3f214f933d71de68272dbd9c7979b43daa9
Noah Reilly <noahreilly08@gmail.com>
2015-02-07 10:21:58 -0500

    maybe fixed backup

--- src/org/usfirst/frc/team1885/robot/modules/drivetrain/BackupRoutine.java
+++ src/org/usfirst/frc/team1885/robot/modules/drivetrain/BackupRoutine.java
@@ -6,46 +6,36 @@ import org.usfirst.frc.team1885.robot.input.SensorInputControl;
 import org.usfirst.frc.team1885.robot.output.RobotControl;
 
 public class BackupRoutine {
-    private PID rightDistanceControlLoop;
-    private PID leftDistanceControlLoop;
-    private double distance;
-    private double error;
+    private PID distanceControlLoop;
+    private final double ERROR = 100;
     private double leftDriveOutput;
-    private double rightDriveOutput; 
-    private double distanceTraveled;
+    private double rightDriveOutput;
+    private double distanceFromTarget;
+    private static BackupRoutine instance;
     // lidar output in centimeters, distance var input needs to match
-    public BackupRoutine(double d, double e) {
-        rightDistanceControlLoop = new PID(0.01, 0.00001, 0);
-        leftDistanceControlLoop = new PID(0.01, 0.00001, 0);
-        distance = d;
-        error = e;
+    protected BackupRoutine() {
+        distanceControlLoop = new PID(0.01, 0.00001, 0);
         reset();
     }
-    public void update() {
-        System.out.println("AutoDriveFwd::[left dist, right dist] " + distanceTraveled + ", " + distanceTraveled);
-        
-        if (Math.abs(distanceTraveled  - distance) <= error && Math.abs(distanceTraveled  - distance) <= error ) {
-            this.reset();
+    public static BackupRoutine getInstance(){
+        if( instance == null ){
+            instance = new BackupRoutine();
         }
-        
-        distanceTraveled = SensorInputControl.getInstance().getLidarSensor(SensorType.LIDAR).getDistance();
-        if(Math.abs(distanceTraveled  - distance) > error) {
-            leftDriveOutput = leftDistanceControlLoop.getPID(distance, distanceTraveled);
-            rightDriveOutput = rightDistanceControlLoop.getPID(distance, distanceTraveled);
+        return instance;
+    }
+    public void update(double leftJoystick, double rightJoystick) {
+        if(distanceFromTarget < ERROR) {
+            distanceFromTarget = SensorInputControl.getInstance().getLidarSensor(SensorType.LIDAR).getDistance();
+            leftDriveOutput = distanceControlLoop.getPID(ERROR, distanceFromTarget);
+            rightDriveOutput = leftDriveOutput;
         } else {
-            leftDistanceControlLoop.reset();
-            leftDriveOutput = 0;
-            rightDistanceControlLoop.reset();
-            rightDriveOutput = 0;
+            leftDriveOutput = leftJoystick;
+            rightDriveOutput = rightJoystick;
+            reset();
         }
-        
-        System.out.println("AutoDriveFwd::[left speed, right speed] " + leftDriveOutput + ", " + rightDriveOutput);
-        
         RobotControl.getInstance().updateDriveSpeed(-leftDriveOutput, -rightDriveOutput);
     }
     public void reset() {
-        rightDistanceControlLoop.reset();
-        leftDistanceControlLoop.reset();
-        RobotControl.getInstance().updateDriveSpeed(0, 0);
+        distanceControlLoop.reset();
     }
 }
