f1d700c148ca38c2a5e0873501c9064ac2cfbcf3
Noah Reilly <noahreilly08@gmail.com>
2015-02-15 17:52:50 -0500

    added push light troll thing

--- src/org/usfirst/frc/team1885/robot/modules/drivetrain/Alignment.java
+++ src/org/usfirst/frc/team1885/robot/modules/drivetrain/Alignment.java
@@ -13,33 +13,41 @@ public class Alignment implements Module {
     private Value left;
     private Value right;
     private static Alignment instance;
-    
-    protected Alignment(){
+
+    protected Alignment() {
         left = Value.kOff;
         right = Value.kOff;
     }
-    
-    public static Alignment getInstance(){
-        if(instance == null){
+
+    public static Alignment getInstance() {
+        if (instance == null) {
             instance = new Alignment();
         }
         return instance;
     }
-    
+
     @Override
     public void update() {
-        if(SensorInputControl.getInstance().isActive(SensorType.TOUCH_SENSOR_TOTE_LEFT)){
+        if (SensorInputControl.getInstance().isActive(
+                SensorType.TOUCH_SENSOR_TOTE_LEFT)) {
             left = Value.kOn;
+        } else {
+            left = Value.kOff;
         }
-        if(SensorInputControl.getInstance().isActive(SensorType.TOUCH_SENSOR_TOTE_RIGHT)){
+        if (SensorInputControl.getInstance().isActive(
+                SensorType.TOUCH_SENSOR_TOTE_RIGHT)) {
             right = Value.kOn;
+        } else {
+            right = Value.kOff;
         }
     }
 
     @Override
     public void updateOutputs() {
-        RobotControl.getInstance().updateRelay(RobotMotorType.ALIGNMENT_LEFT, left);
-        RobotControl.getInstance().updateRelay(RobotMotorType.ALIGNMENT_RIGHT, right);
+        RobotControl.getInstance().updateRelay(RobotMotorType.ALIGNMENT_LEFT,
+                left);
+        RobotControl.getInstance().updateRelay(RobotMotorType.ALIGNMENT_RIGHT,
+                right);
     }
 
 }
