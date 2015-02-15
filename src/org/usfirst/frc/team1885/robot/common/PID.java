f7b4fb1fd0b40785996000371be78a4f24bf9dc5
rudicio <rudicio@gmail.com>
2015-02-14 23:14:39 -0500

    added scaling to PID

--- src/org/usfirst/frc/team1885/robot/common/PID.java
+++ src/org/usfirst/frc/team1885/robot/common/PID.java
@@ -6,6 +6,7 @@ public class PID {
 	private final double d;
 	private double error, previousError;
 	private double integral;
+	private boolean scaleOutput;
 	
 	public PID(double inputP, double inputI, double inputD) {
 		p = inputP;
@@ -13,6 +14,7 @@ public class PID {
 		d = inputD;
 		error = 0.0;
 		integral = 0.0;
+		scaleOutput = true;
 	}
 
 	public void reset() {
@@ -21,11 +23,17 @@ public class PID {
 		integral = 0;
 	}
 
-	public double getPID (double projectedSpeed, double currentSpeed) {
+	public double getPID (double projectedValue, double currentValue) {
 		previousError = error;
-		error = projectedSpeed - currentSpeed;
+		error = projectedValue - currentValue;
 		
-		return (p * getP()) + (i * getI(1.0)) + (d * getD());
+		double output = (p * getP()) + (i * getI(1.0)) + (d * getD());
+		
+		if(scaleOutput) {
+			output = output / projectedValue;
+		}
+		
+		return output;
 	}
 
 	public double getP() {
