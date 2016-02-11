8d8ce1048154fd3a614f134fe9e94bc0bb7969ad
FRCProgramming1 <juanes051097@gmail.com>
2016-02-11 13:52:03 -0500

    Gyro testing

--- src/org/usfirst/frc/team1885/robot/Robot.java
+++ src/org/usfirst/frc/team1885/robot/Robot.java
@@ -13,67 +13,66 @@ import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Timer;
 
-public class Robot extends SampleRobot 
-{
+public class Robot extends SampleRobot {
     private RobotControlWithSRX srx;
     private DriverInputControlSRX drx;
     private SensorInputControlSRX sensorrx;
     private AHRS pengisbad;
-    public Robot()
-    {
-        pengisbad = new AHRS(SerialPort.Port.kMXP); 
+
+    public Robot() {
+        pengisbad = new AHRS(SerialPort.Port.kMXP);
         System.err.println("I am ROBOT!!");
         RobotConfigSRX.configureRobot();
         srx = RobotControlWithSRX.getInstance();
         drx = DriverInputControlSRX.getInstance();
         this.sensorrx = SensorInputControlSRX.getInstance();
     }
-    public void operatorControl()
-    {
+    public void operatorControl() {
         System.err.println("HAS OPERATOR CONTROL");
-        PressureSensor ps = new PressureSensor(0);
-        while(isOperatorControl() && isEnabled())
-        {
+        // PressureSensor ps = new PressureSensor(0);
+        while (isOperatorControl() && isEnabled()) {
             StringBuilder output = new StringBuilder();
-            output.append("\n" + ps.getPressure());
-//            drx.update();
-//            sensorrx.update();
-           /* 
-            //can't update faster than this for motors
-            StringBuilder output = new StringBuilder();
-            output.append("test");
-            output.append(  "\nIMU_Connected" +      pengisbad.isConnected());
-            output.append(  "\nIMU_IsCalibrating" +    pengisbad.isCalibrating());
-            output.append(   "\nIMU_Yaw" +              pengisbad.getYaw());
-            output.append(   "\nIMU_Pitch" +            pengisbad.getPitch());
-            output.append(   "\nIMU_Roll" +             pengisbad.getRoll());
-            
-            output.append(   "\nRawGyro_X" +            pengisbad.getRawGyroX());
-            output.append(   "\nRawGyro_Y" +           pengisbad.getRawGyroY());
-            output.append(   "\nRawGyro_Z" +           pengisbad.getRawGyroZ());
-            output.append(   "\nRawAccel_X"+           pengisbad.getRawAccelX());
-            output.append(   "\nRawAccel_Y"+           pengisbad.getRawAccelY());
-            output.append(   "\nRawAccel_Z" +           pengisbad.getRawAccelZ());
-            output.append(   "\nRawMag_X" +          pengisbad.getRawMagX());
-            output.append(   "\nRawMag_Y" +             pengisbad.getRawMagY());
-            output.append(   "\nRaw2Mag_Z" +             pengisbad.getRawMagZ());
-            output.append(   "\nIMU_Temp_C" +           pengisbad.getTempC());
+            // output.append("\n" + ps.getPressure());
+            // drx.update();
+            // sensorrx.update();
 
-            output.append(   "\nIMU_Accel_X" +          pengisbad.getWorldLinearAccelX());
-            output.append(   "\nIMU_Accel_Y" +         pengisbad.getWorldLinearAccelY());
-            output.append(  "\nIMU_IsMoving" +       pengisbad.isMoving());
-            output.append(  "\nIMU_IsRotating" +       pengisbad.isRotating());
-            
-*/            
+            // can't update faster than this for motors
+            // output.append("test");
+            // output.append( "\nIMU_Connected" + pengisbad.isConnected());
+            // output.append( "\nIMU_IsCalibrating" +
+            // pengisbad.isCalibrating());
+
+            output.append("\nIMU_Yaw" + pengisbad.getYaw());
+            output.append("\nIMU_Pitch" + pengisbad.getPitch());
+            output.append("\nIMU_Roll" + pengisbad.getRoll());
+
+            output.append("\nRawGyro_X" + pengisbad.getRawGyroX());
+            output.append("\nRawGyro_Y" + pengisbad.getRawGyroY());
+            output.append("\nRawGyro_Z" + pengisbad.getRawGyroZ());
+            /*
+             * output.append( "\nRawAccel_X"+ pengisbad.getRawAccelX());
+             * output.append( "\nRawAccel_Y"+ pengisbad.getRawAccelY());
+             * output.append( "\nRawAccel_Z" + pengisbad.getRawAccelZ());
+             * output.append( "\nRawMag_X" + pengisbad.getRawMagX());
+             * output.append( "\nRawMag_Y" + pengisbad.getRawMagY());
+             * output.append( "\nRaw2Mag_Z" + pengisbad.getRawMagZ());
+             * output.append( "\nIMU_Temp_C" + pengisbad.getTempC());
+             * 
+             * output.append( "\nIMU_Accel_X" +
+             * pengisbad.getWorldLinearAccelX()); output.append( "\nIMU_Accel_Y"
+             * + pengisbad.getWorldLinearAccelY()); output.append(
+             * "\nIMU_IsMoving" + pengisbad.isMoving()); output.append(
+             * "\nIMU_IsRotating" + pengisbad.isRotating());
+             * 
+             */
             DriverStation.reportError(output.toString(), false);
             Timer.delay(1);
         }
-            
-    }
-//    public void autonomous()
-//    {
-//        AutoRoutine ar = new AutoRoutine(this);
-//        ar.execute();
-//    }
-}
 
+    }
+    // public void autonomous()
+    // {
+    // AutoRoutine ar = new AutoRoutine(this);
+    // ar.execute();
+    // }
+}
