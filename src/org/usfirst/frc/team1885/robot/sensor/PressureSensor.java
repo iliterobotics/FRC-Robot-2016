0f9b5af5dfcd8b6b4e7de0080a4980b0bd7872d8
kianmehrabani <kianmehrabani@gmail.com>
2016-02-13 11:25:52 -0500

    Reconfigured PressureSensor for normalization.

--- src/org/usfirst/frc/team1885/robot/sensor/PressureSensor.java
+++ src/org/usfirst/frc/team1885/robot/sensor/PressureSensor.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team1885.robot.sensor;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class PressureSensor extends AnalogInput {
-
-    public static final double battery_voltage = 5.0;
+    
+    public static final int NORMALIZED_PRESSURE = 120;
+    public static final double BATTERY_VOLTAGE = 5.0;
     public PressureSensor(int channel) {
         super(channel);
     }
     public double getPressure()
     {
-        return (250.0 * (getVoltage()/battery_voltage)) - 25;
+        return (250.0 * (getVoltage()/BATTERY_VOLTAGE)) - 25;
     }
-
 }
---
+++ src/org/usfirst/frc/team1885/robot/sensor/Switch.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team1885.robot.sensor;
+
+public class Switch {
+
+}
