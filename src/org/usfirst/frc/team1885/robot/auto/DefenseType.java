5f6834cd4c2aac4ff5355a27a2c6f7960c26fbf7
forteddyt <forteddyt@gmail.com>
2016-02-13 19:28:04 -0500

    Added JavaDoc of DefenseType enum

--- src/org/usfirst/frc/team1885/robot/auto/DefenseType.java
+++ src/org/usfirst/frc/team1885/robot/auto/DefenseType.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team1885.robot.auto;
 
+/**
+ * Enumeration of all defenses in game.
+ * 
+ * @author ILITE Robotics
+ * @version <2/13/2016>
+ */
 public enum DefenseType {
     LOWBAR, PORTCULLIS, CHEVAL, MOAT, RAMPARTS, DRAWBRIDGE, SALLYPORT, ROCKWALL, ROUGHTERRAIN
 }
