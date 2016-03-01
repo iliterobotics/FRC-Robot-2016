051019c26b174de21bb5649bc191f8b9f80c7ad9
KellyMichael <rockymkelly@gmail.com>
2016-03-01 12:33:26 -0500

    started integrating server data classes

---
+++ src/org/usfirst/frc/team1885/serverdata/ServerInformation.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team1885.serverdata;
+
+public class ServerInformation {
+    public static final String DEFAULT_LAPTOP_WEB_ADDRESS = "http://10.18.85.207:8083";
+}
--- src/org/usfirst/frc/team1885/serverdata/ShooterDataClient.java
+++ src/org/usfirst/frc/team1885/serverdata/ShooterDataClient.java
@@ -1,22 +1,31 @@
-//package org.usfirst.frc.team1885.serverdata;
-//
-//import dataclient.DataServerWebClient;
-//import dataclient.robotdata.vision.HighGoal;
-//
-//public class ShooterDataClient {
-//    
-//    private static final String DEFAULT_LAPTOP_WEB_ADDRESS = "http://10.18.85.207:8083";
-//    
-//    private DataServerWebClient client;
-//    private HighGoal highGoalData;
-//    
-//    public ShooterDataClient(){
-//        client = new DataServerWebClient(DEFAULT_LAPTOP_WEB_ADDRESS);
-//        highGoalData = new HighGoal(client);
-//        client.watch(highGoalData, updateData -> updateData());
-//    }
-//    
-//    private void updateData(){
-//        
-//    }
-//}
+package org.usfirst.frc.team1885.serverdata;
+
+import org.usfirst.frc.team1885.robot.output.RobotControlWithSRX;
+
+import dataclient.DataServerWebClient;
+import dataclient.robotdata.vision.HighGoal;
+
+public class ShooterDataClient {
+    
+    private DataServerWebClient client;
+    private HighGoal highGoalData;
+    
+    public static ShooterDataClient startShooterDataClient(){
+        return new ShooterDataClient();
+    }
+    
+    private ShooterDataClient(){
+        client = new DataServerWebClient(ServerInformation.DEFAULT_LAPTOP_WEB_ADDRESS);
+        highGoalData = new HighGoal(client);
+        client.watch(highGoalData, updateData -> updateData());
+    }
+    
+    
+    private void updateData(){
+        //TODO calculate tilt and twist
+        double tiltAngle = 0;
+        double twistAngle = 0;
+        RobotControlWithSRX.getInstance().updateShooterTilt(tiltAngle);
+        RobotControlWithSRX.getInstance().updateShooterTilt(twistAngle);
+    }
+}
