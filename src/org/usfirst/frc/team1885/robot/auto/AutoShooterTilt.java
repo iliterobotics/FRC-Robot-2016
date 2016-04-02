619674221dc703ed938ab7d6718f3ad5796e8332
FRC DriverStation <>
2016-04-02 09:57:27 -0400

    removed incrementation logic

--- src/org/usfirst/frc/team1885/robot/auto/AutoShooterTilt.java
+++ src/org/usfirst/frc/team1885/robot/auto/AutoShooterTilt.java
@@ -1,34 +1,20 @@
 package org.usfirst.frc.team1885.robot.auto;
 
-import org.usfirst.frc.team1885.robot.modules.ActiveIntake;
 import org.usfirst.frc.team1885.robot.modules.ModuleControl;
 import org.usfirst.frc.team1885.robot.modules.Shooter;
 
-import edu.wpi.first.wpilibj.DriverStation;
-
 public class AutoShooterTilt extends AutoCommand {
-    private static final double ERROR = 1;
     private final double angle;
-    private double currentAngle;    
     public AutoShooterTilt(double angle) {
         this.angle = angle;
     }
     @Override
     public boolean init() {
-        currentAngle = Shooter.getInstance().getRelativeTilt();
         return true;
     }
     @Override
     public boolean execute() {
-        currentAngle = Shooter.getInstance().getRelativeTilt();
-        if(Math.abs(currentAngle - angle) <= ERROR){
-            return true;
-        }
-        if(currentAngle < angle)
-            currentAngle += Shooter.TILT_MOVEMENT_PROPORTION;
-        else
-           currentAngle -= Shooter.TILT_MOVEMENT_PROPORTION;
-        Shooter.getInstance().setToTiltValue(/*currentAngle*/this.angle);
+        Shooter.getInstance().setToTiltValue(this.angle);
         return false;
     }
     @Override
--- src/org/usfirst/frc/team1885/robot/auto/AutoShooterTwist.java
+++ src/org/usfirst/frc/team1885/robot/auto/AutoShooterTwist.java
@@ -1,35 +1,21 @@
 package org.usfirst.frc.team1885.robot.auto;
 
-import org.usfirst.frc.team1885.robot.modules.ActiveIntake;
 import org.usfirst.frc.team1885.robot.modules.ModuleControl;
 import org.usfirst.frc.team1885.robot.modules.Shooter;
 
-import edu.wpi.first.wpilibj.DriverStation;
-
 public class AutoShooterTwist extends AutoCommand {
-    private static final double ERROR = 0.5;
     private final double angle;
-    private double currentAngle;    
     
     public AutoShooterTwist(double angle) {
         this.angle = angle;
     }
     @Override
     public boolean init() {
-        currentAngle = Shooter.getInstance().getRelativeTwist();
         return true;
     }
     @Override
     public boolean execute() {
-        currentAngle = Shooter.getInstance().getRelativeTwist();
-        if(Math.abs(currentAngle - angle) <= ERROR){
-            return true;
-        }
-        if(currentAngle < angle)
-            currentAngle += Shooter.TWIST_MOVEMENT_PROPORTION;
-        else
-           currentAngle -= Shooter.TWIST_MOVEMENT_PROPORTION;
-        Shooter.getInstance().setToTwistValue(currentAngle);
+        Shooter.getInstance().setToTwistValue(this.angle);
         return false;
     }
     @Override
