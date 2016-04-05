23236d48283d915c5d25539f4d445160595c2ed0
FRC DriverStation <>
2016-04-04 20:16:17 -0400

    added framework for multiple PIDs

--- src/org/usfirst/frc/team1885/robot/modules/UtilityArm.java
+++ src/org/usfirst/frc/team1885/robot/modules/UtilityArm.java
@@ -20,12 +20,17 @@ public class UtilityArm implements Module {
     private boolean preIncUp;
 
     private static int[] positions = { 0 }; // in degree
+    private double movementDirection;
 
-    private static final double CONVERSION_FACTOR = 4096 / 360;
+    private final double CONVERSION_FACTOR = 4096 / 360;
 
-    private static final double ARM_P = 1.0;
-    private static final double ARM_I = 0;
-    private static final double ARM_D = 0;
+    private final double ARM_P = 1.0;
+    private final double ARM_I = 0;
+    private final double ARM_D = 0;
+    
+    private final double ARM_P_DOWN = 1.0;
+    private final double ARM_I_DOWN = 0;
+    private final double ARM_D_DOWN = 0;
 
     private static UtilityArm instance;
 
@@ -40,10 +45,12 @@ public class UtilityArm implements Module {
         RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).changeControlMode(TalonControlMode.Position);
         RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).setFeedbackDevice(FeedbackDevice.QuadEncoder);
         RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).setPID(ARM_P, ARM_I, ARM_D);
+        movementDirection = 1.0;
     }
 
     @Override
     public void update() {
+        double oldCyclePos = currCyclePos;
         if (DriverInputControlSRX.getInstance().getButton(RobotButtonType.UTILITY_ARM_RESET)) {
             currCyclePos = 0;
         }
@@ -52,10 +59,17 @@ public class UtilityArm implements Module {
             currCyclePos = currCyclePos > positions.length ? 1 : currCyclePos + 1;
         }
         preIncUp = DriverInputControlSRX.getInstance().getButton(RobotButtonType.UTILITY_ARM_CYCLE);
+        
+        movementDirection = oldCyclePos < currCyclePos ? 1.0: -1.0;
     }
 
     @Override
     public void updateOutputs() {
+        if(movementDirection < 0){
+            RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).setPID(ARM_P_DOWN, ARM_I_DOWN, ARM_D_DOWN);
+        } else{
+            RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).setPID(ARM_P, ARM_I, ARM_D);
+        }
         RobotControlWithSRX.getInstance().getTalons().get(RobotMotorType.UTILITY_ARM).set(positions[currCyclePos] * CONVERSION_FACTOR);
     }
 
