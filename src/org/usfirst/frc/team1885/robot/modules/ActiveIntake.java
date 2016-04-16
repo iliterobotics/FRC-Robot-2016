d90b92e8b6d9f6c758fb0bef8414376bcce4799f
Noah Reilly <noahreilly08@gmail.com>
2016-04-16 09:54:49 -0400

    cleaned up active intake

--- src/org/usfirst/frc/team1885/robot/modules/ActiveIntake.java
+++ src/org/usfirst/frc/team1885/robot/modules/ActiveIntake.java
@@ -11,18 +11,19 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 public class ActiveIntake implements Module {
 
-    public static final double INTAKE_SPEED = -1;
     private static ActiveIntake instance;
-    private double intakeSpeed;
-    private DoubleSolenoid.Value isIntaking;
-    private MotorState intakeState;
-    private DriverInputControlSRX driverInputControl;
-    private RobotControlWithSRX robotControl;
-    private double counter;
-    private boolean previousIntakeToggle;
-    private static final double delay = 1000;
+    
+    public static final double INTAKE_SPEED = -1;
     public static final DoubleSolenoid.Value intakeUp = DoubleSolenoid.Value.kForward;
     public static final DoubleSolenoid.Value intakeDown = DoubleSolenoid.Value.kReverse;
+    
+    private DriverInputControlSRX driverInputControl;
+    private RobotControlWithSRX robotControl;
+    
+    private double intakeSpeed;
+    private MotorState intakeState;
+    private DoubleSolenoid.Value isIntaking;
+    private boolean previousIntakeToggle;
 
     private ActiveIntake() {
         driverInputControl = DriverInputControlSRX.getInstance();
@@ -31,7 +32,6 @@ public class ActiveIntake implements Module {
         isIntaking = intakeUp;
         reset();
         previousIntakeToggle = false;
-        DriverStation.reportError("" + isDown(), false);
     }
     public static ActiveIntake getInstance() {
         if (instance == null) {
@@ -39,43 +39,36 @@ public class ActiveIntake implements Module {
         }
         return instance;
     }
-    public void init(){
-        
-    }
-    public void setMotorState(MotorState intakeState) {
-        this.intakeState = intakeState;
-    }
+    public void init() {}
+
     public MotorState getIntakeMotorState() {
         return intakeState;
     }
     public double getIntakeSpeed() {
         return intakeSpeed;
     }
-    public void updateIntake() {
+    public void update() {
         intakeSpeed = 0;
         if ((driverInputControl.getButton(RobotButtonType.INTAKE_IN))) {
-                    intakeState = MotorState.REVERSE;
-                    intakeSpeed = -INTAKE_SPEED;
-                    DriverStation.reportError("\nIntaking", false);
+            intakeState = MotorState.REVERSE;
+            intakeSpeed = -INTAKE_SPEED;
+            DriverStation.reportError("\nIntaking", false);
         }
 
         if ((driverInputControl.getButton(RobotButtonType.INTAKE_OUT))) {
             intakeState = MotorState.FORWARD;
             intakeSpeed = INTAKE_SPEED;
         }
-//        DriverStation.reportError("Solenoid Button State " + driverInputControl.getButton(RobotButtonType.INTAKE_SOLENOID) 
-//        + "\n", false);
-//        DriverStation.reportError("  Solenoid State " + isIntaking + "\n", false);
-        if (driverInputControl.getButton(RobotButtonType.INTAKE_SOLENOID)
-                && !previousIntakeToggle ) {
+
+        if (driverInputControl.getButton(RobotButtonType.INTAKE_SOLENOID) && !previousIntakeToggle) {
             isIntaking = isIntaking == intakeUp ? intakeDown : intakeUp;
         }
         previousIntakeToggle = driverInputControl.getButton(RobotButtonType.INTAKE_SOLENOID);
-        
-        if(Shooter.getInstance().getRelativeTilt() > Shooter.LOWER_TILT_COLLISION && Shooter.getInstance().getRelativeTilt() < Shooter.UPPER_TILT_COLLISION){
+
+        if (Shooter.getInstance().getRelativeTilt() > Shooter.LOWER_TILT_COLLISION && Shooter.getInstance().getRelativeTilt() < Shooter.UPPER_TILT_COLLISION) {
             isIntaking = intakeDown;
         }
-        
+
         updateIntake(intakeSpeed);
     }
     public void updateIntake(double intakeSpeed) {
@@ -91,27 +84,22 @@ public class ActiveIntake implements Module {
     public void reset() {
         this.intakeState = MotorState.OFF;
         intakeSpeed = 0;
-        isIntaking = intakeUp;        
+        isIntaking = intakeUp;
         updateOutputs();
     }
 
     public void updateOutputs() {
-//        DriverStation.reportError("\nIntake Motor Speed " + intakeSpeed + "\nSolenoid State" + isIntaking, false);
         robotControl.updateIntakeMotor(intakeSpeed);
-        robotControl.updateDoubleSolenoid(RobotPneumaticType.INTAKE_SETTER, isIntaking);  
+        robotControl.updateDoubleSolenoid(RobotPneumaticType.INTAKE_SETTER, isIntaking);
     }
-    @Override
-    public void update() {
-        updateIntake();
-    }
-    public void setIntakeSolenoid(DoubleSolenoid.Value input){
-//        DriverStation.reportError("\n Intake Position" + input, false);
+
+    public void setIntakeSolenoid(DoubleSolenoid.Value input) {
         isIntaking = input;
     }
-    public void setIntakeSpeed(double speed){
+    public void setIntakeSpeed(double speed) {
         intakeSpeed = speed;
     }
-    public boolean isDown(){
+    public boolean isDown() {
         return isIntaking == intakeDown;
     }
 }
