100739a4d1a56f47205f7c815c0210e9a472ed3e
Noah Reilly <noahreilly08@gmail.com>
2016-04-16 09:51:14 -0400

    cleaned up inputs

--- src/org/usfirst/frc/team1885/robot/input/DriverInputControlSRX.java
+++ src/org/usfirst/frc/team1885/robot/input/DriverInputControlSRX.java
@@ -9,7 +9,6 @@ import org.usfirst.frc.team1885.robot.common.type.RobotJoystickType;
 import org.usfirst.frc.team1885.robot.modules.Shooter;
 import org.usfirst.frc.team1885.robot.modules.drivetrain.DrivetrainControl;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Joystick.AxisType;
 
@@ -52,21 +51,17 @@ public class DriverInputControlSRX {
         return true;
     }
     public boolean getButton(RobotButtonType buttonType) {
-        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap
-                .getInstance().getButtonMap().get(buttonType);
+        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap.getInstance().getButtonMap().get(buttonType);
         if (buttonMatch != null) {
-            return getJoystick(buttonMatch.getJoystickType())
-                    .getRawButton(buttonMatch.getPort());
+            return getJoystick(buttonMatch.getJoystickType()).getRawButton(buttonMatch.getPort());
         }
         return false;
     }
 
     public int getPOVButton(RobotButtonType buttonType) {
-        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap
-                .getInstance().getButtonMap().get(buttonType);
+        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap.getInstance().getButtonMap().get(buttonType);
 
-        int povValue = getJoystick(buttonMatch.getJoystickType())
-                .getPOV(buttonMatch.getPort());
+        int povValue = getJoystick(buttonMatch.getJoystickType()).getPOV(buttonMatch.getPort());
 
         if (povValue > -1) {
             return povValue;
@@ -76,76 +71,33 @@ public class DriverInputControlSRX {
     }
 
     public double getPressureButton(RobotButtonType buttonType) {
-        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap
-                .getInstance().getButtonMap().get(buttonType);
+        JoystickButtonMatch buttonMatch = (JoystickButtonMatch) JoystickButtonMap.getInstance().getButtonMap().get(buttonType);
         double rawAxis = 0;
         try {
-            rawAxis = getJoystick(buttonMatch.getJoystickType())
-                    .getRawAxis(buttonMatch.getAxisType().value);
+            rawAxis = getJoystick(buttonMatch.getJoystickType()) .getRawAxis(buttonMatch.getAxisType().value);
         } catch (Exception e) {
-            System.err.println("EXCEPTION on buttonType= " + buttonType);
             e.printStackTrace();
         }
         return rawAxis;
     }
-    public static double expScale(double axis) {
-        double A = 1.85; // X^3 coefficient
-
-        double B = -0.7; // X^2 coefficient
-
-        // X coefficient - this correlates to the approximate linear increase
-        // during low-inputs (i.e. on the graph, 0.1 < x < 0.4). This creates a
-        // "low power band" that is easy for the driver to find, where the robot
-        // responds at its minimum practical power to allow for fine-tuned
-        // movements
-        double C = 0.2;
-
-        // Assume the variable Y_AXIS is represents the joystick axis input
-        double ABS_Y = Math.abs(axis);
-        double AXIS_SIGN = 1.0;
-        if (axis < 0)
-            AXIS_SIGN = -1.0;
-
-        // Account for the Deadband, Max Power band, and Min Torque band, but
-        // exponentially scale it
-        double T_D_EXP = 0; // default 0, only check for outside deadband cases
-        if (ABS_Y >= DEADZONE) {
-            // Note - T_D_EXP is > 1.0 when ABS_Y > JOYSTICK_FULL_POWER_BAND, so
-            // no need to deal with that case specifically
-            T_D_EXP = A * (Math.pow(ABS_Y - DEADZONE, 3))
-                    + B * (Math.pow(ABS_Y - DEADZONE, 2))
-                    + C * (ABS_Y - DEADZONE) + MIN_TORQUE_RESPONSE;
-        }
-        T_D_EXP = Math.min(T_D_EXP, 1.0) * AXIS_SIGN;
-        return T_D_EXP;
-    }
 
     public void update() {
         this.update(
-                this.getJoystick(RobotJoystickType.LEFT_DRIVE)
-                        .getAxis(AxisType.kY),
-                this.getJoystick(RobotJoystickType.RIGHT_DRIVE)
-                        .getAxis(AxisType.kY));
+                this.getJoystick(RobotJoystickType.LEFT_DRIVE) .getAxis(AxisType.kY),
+                this.getJoystick(RobotJoystickType.RIGHT_DRIVE) .getAxis(AxisType.kY));
     }
     public void update(double leftJoystick, double rightJoystick) {
-//        System.err.println("LEFT= " + leftJoystick + ", RIGHT= " + rightJoystick);
         this.rightDriveSpeed = deadzone(rightJoystick);
         this.leftDriveSpeed = deadzone(leftJoystick);
         DrivetrainControl.getInstance().setLeftDriveSpeed(leftDriveSpeed);
         DrivetrainControl.getInstance().setRightDriveSpeed(rightDriveSpeed);
     }
     public double getControllerTwist() {
-        return this.getJoystick(RobotJoystickType.CONTROLLER)
-                .getAxis(AxisType.kTwist);
+        return this.getJoystick(RobotJoystickType.CONTROLLER).getAxis(AxisType.kTwist);
     }
 
     public double getControllerThrottle() {
-        return this.getJoystick(RobotJoystickType.CONTROLLER)
-                .getAxis(AxisType.kThrottle);
-    }
-
-    public boolean isResetButtonDown() {
-        return this.getButton(RobotButtonType.RESET_BUTTON);
+        return this.getJoystick(RobotJoystickType.CONTROLLER).getAxis(AxisType.kThrottle);
     }
 
     public boolean isButtonDown(RobotButtonType type) {
